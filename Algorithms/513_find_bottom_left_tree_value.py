4d3fc833035d348f5244e73d4d9944ee7399c5ea
[kashinkou] <[kashinkou@gmail.com]>
2018-03-20 20:23:25 -0700

    pokelove

---
+++ Algorithms/513_find_bottom_left_tree_value.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findBottomLeftValue(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+
+        queue = collections.deque([root])
+        while queue:
+            level = []
+            n = len(queue)
+            for i in xrange(n):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return level[0]
